7a5792e43b5604e69046510c288901d6d36fbc2a
Yuyu <yuchi@xfers.com>
2022-08-18 22:13:59 +0800

    feat: 🎸 Update email address

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://pedantic-bhaskara-fa0a7e.netlify.app/',
-  pathPrefix: '/',
-  title: 'Blog by Yuyu',
-  subtitle: '',
-  copyright: '© All rights reserved.',
-  disqusShortname: 'tulā tapas ca',
+  url: "https://pedantic-bhaskara-fa0a7e.netlify.app/",
+  pathPrefix: "/",
+  title: "Blog by Yuyu",
+  subtitle: "",
+  copyright: "© All rights reserved.",
+  disqusShortname: "tulā tapas ca",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-158806891-1',
+  googleAnalyticsId: "UA-158806891-1",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'Gallery',
-      path: '/gallery'
+      label: "Gallery",
+      path: "/gallery",
     },
     // {
     //   label: 'About me',
@@ -29,25 +29,25 @@ module.exports = {
     // }
   ],
   author: {
-    name: 'Yuyu',
-    photo: '/photo.jpg',
-    bio: 'Frontend engineer, love animals and plants.',
+    name: "Yuyu",
+    photo: "/photo.jpg",
+    bio: "Frontend engineer, love animals and plants.",
     contacts: {
-      email: 'yuyu811029@gmail.com',
-      facebook: 'yu.yu.chi.1',
-      telegram: '',
-      twitter: '',
-      github: 'yuyuchi',
-      rss: '',
-      vkontakte: '',
-      linkedin: 'yu-chi-yu-6379a3b1',
-      instagram: 'yuyuchi1029',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "iu.yuyuchi@gmail.com",
+      facebook: "yu.yu.chi.1",
+      telegram: "",
+      twitter: "",
+      github: "yuyuchi",
+      rss: "",
+      vkontakte: "",
+      linkedin: "yu-chi-yu-6379a3b1",
+      instagram: "yuyuchi1029",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
