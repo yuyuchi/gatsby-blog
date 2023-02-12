005f8b224b56679d16391138a34db757732a2a13
Yuyu <yuchi@xfers.com>
2023-02-12 20:50:15 +0800

    feat: 🎸 Edit config

--- config.js
+++ config.js
@@ -1,53 +1,53 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by Yuyu',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://pedantic-bhaskara-fa0a7e.netlify.app/",
+  pathPrefix: "/",
+  title: "Blog by Yuyu",
+  subtitle: "",
+  copyright: "© All rights reserved.",
+  disqusShortname: "tulā tapas ca",
   postsPerPage: 4,
-  // googleAnalyticsId: 'UA-73379983-2', TODO
+  googleAnalyticsId: "UA-158806891-1",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'Gallery',
-      path: '/gallery'
+      label: "Gallery",
+      path: "/gallery",
     },
-    // {
-    //   label: 'About me',
-    //   path: '/pages/about'
-    // },
+    {
+      label: "About",
+      path: "/pages/about",
+    },
     // {
     //   label: 'Contact me',
     //   path: '/pages/contacts'
     // }
   ],
   author: {
-    name: 'Yuyu',
-    photo: '/photo.jpg',
-    bio: 'Frontend engineer, love animals and plants.',
+    name: "Yuyu",
+    photo: "/photo.jpg",
+    bio: "Frontend engineer, love animals and plants.",
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "iu.yuyuchi@gmail.com",
+      facebook: "yu.yu.chi.1",
+      telegram: "",
+      twitter: "",
+      github: "yuyuchi",
+      rss: "",
+      vkontakte: "",
+      linkedin: "yu-chi-yu-6379a3b1",
+      instagram: "yuyuchi1029",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
